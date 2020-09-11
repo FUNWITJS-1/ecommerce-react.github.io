d8dbd5b8a939fa5da4c40960933a4715f4cec8f9
Anamika Katyal <Anamika.Katyal@tatacommunications.com>
2020-09-11 23:12:42 +0530

    Updates

---
+++ precache-manifest.c8f9a70ba42d6de6277211d9e7974d71.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a8b6451405e8d44485d7e0d52052361",
+    "url": "/ecommerce-react.github.io/index.html"
+  },
+  {
+    "revision": "aedc3dc2d60bd112da7e",
+    "url": "/ecommerce-react.github.io/static/css/2.a3cbbf5c.chunk.css"
+  },
+  {
+    "revision": "e9f07635f3c25c307a69",
+    "url": "/ecommerce-react.github.io/static/css/main.69a10009.chunk.css"
+  },
+  {
+    "revision": "aedc3dc2d60bd112da7e",
+    "url": "/ecommerce-react.github.io/static/js/2.480cccf5.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/ecommerce-react.github.io/static/js/2.480cccf5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9f07635f3c25c307a69",
+    "url": "/ecommerce-react.github.io/static/js/main.d18c4d13.chunk.js"
+  },
+  {
+    "revision": "8364b26e5d64c38469b3",
+    "url": "/ecommerce-react.github.io/static/js/runtime-main.fdd88b6b.js"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/ecommerce-react.github.io/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/ecommerce-react.github.io/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/ecommerce-react.github.io/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/ecommerce-react.github.io/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/ecommerce-react.github.io/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/ecommerce-react.github.io/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/ecommerce-react.github.io/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/ecommerce-react.github.io/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/ecommerce-react.github.io/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/ecommerce-react.github.io/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/ecommerce-react.github.io/static/media/notification.c0d3c94c.eot"
+  }
+]);
